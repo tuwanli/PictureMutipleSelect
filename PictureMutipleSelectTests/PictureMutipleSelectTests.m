dd7ef6f7a4bbed990f967bb3731c6584c4c70abb
tuwanli <909111806@qq.com>
2015-12-08 15:43:48 +0800

    Initial Commit

---
+++ PictureMutipleSelectTests/PictureMutipleSelectTests.m
@@ -0,0 +1,40 @@
+//
+//  PictureMutipleSelectTests.m
+//  PictureMutipleSelectTests
+//
+//  Created by 涂婉丽 on 15/12/8.
+//  Copyright (c) 2015年 涂婉丽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface PictureMutipleSelectTests : XCTestCase
+
+@end
+
+@implementation PictureMutipleSelectTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
